352663a3c034ee2b36787a3a93e82c055d215a31
matwszedybyl <mwszedybyl@yahoo.com>
2014-05-06 14:57:37 -0400

    starting over

---
+++ src/com/riis/ProgrammingChallengeActivity.java
@@ -0,0 +1,15 @@
+package com.riis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProgrammingChallengeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

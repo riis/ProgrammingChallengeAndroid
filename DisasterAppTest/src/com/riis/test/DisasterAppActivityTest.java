cda93a89a3d6cb5778cdc0c493eee5041fd07381
TEK292 <tkocikjr@comcast.net>
2014-05-28 13:45:01 -0400

    DA-23: Created the Add New Contact button and the navigation logic

---
+++ DisasterAppTest/src/com/riis/test/DisasterAppActivityTest.java
@@ -0,0 +1,51 @@
+package com.riis.test;
+
+import android.app.Instrumentation.ActivityMonitor;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.riis.DisasterAppActivity;
+import com.riis.NewContactActivity;
+import com.riis.R;
+
+public class DisasterAppActivityTest 
+	extends ActivityInstrumentationTestCase2<DisasterAppActivity> {
+	
+	private Button createContactScreenButton;
+	private TextView sampleLabel;
+
+	public DisasterAppActivityTest() {
+		super(DisasterAppActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		DisasterAppActivity disasterAppActivity = getActivity();
+		
+		createContactScreenButton = (Button) disasterAppActivity.findViewById(R.id.createContactScreenButton);
+		sampleLabel = (TextView) disasterAppActivity.findViewById(R.id.sampleLabel);
+	}
+	
+	public void testSampleLabelExists() {
+		assertNotNull(sampleLabel);
+	}
+	
+	public void testCreateContactScreenButtonExists() {
+		assertNotNull(createContactScreenButton);
+	}
+	
+	public void testCreateContactButtonIntent() {
+		ActivityMonitor monitor = getInstrumentation().addMonitor(NewContactActivity.class.getName(), null, true);
+		
+		TouchUtils.clickView(this, ((View) createContactScreenButton));
+		
+		monitor.waitForActivityWithTimeout(2000);
+		assertEquals(1, monitor.getHits());
+		
+		getInstrumentation().removeMonitor(monitor);
+	}
+}

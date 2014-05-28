e593452da35d0fc1edd7671c85c31022685f2d4d
matwszedybyl <mwszedybyl@yahoo.com>
2014-05-28 15:16:50 -0400

    adding new contact files

---
+++ DisasterAppActivity/src/com/riis/NewContactActivity.java
@@ -0,0 +1,18 @@
+package com.riis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewContactActivity extends Activity
+{
+    /** Called when the new contact button is pressed. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newcontact);
+    }
+    
+    
+    
+}

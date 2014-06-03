571ea791c82831a6e539f8da20645d7b304d787c
TEK292 <tkocikjr@comcast.net>
2014-06-03 16:29:09 -0400

    Fixed repo again?

---
+++ DisasterAppTest/src/com/riis/models/test/ContactTest.java
@@ -0,0 +1,60 @@
+package com.riis.models.test;
+
+import junit.framework.TestCase;
+
+import com.riis.models.Contact;
+
+public class ContactTest extends TestCase {
+	
+	private Contact newContact;
+
+	public ContactTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		newContact = new Contact(1);
+	}
+	
+	public void testNewFirstName() {
+		newContact.setFirstName("Bob");
+		assertEquals("Bob", newContact.getFirstName());
+	}
+	
+	public void testNewLastName() {
+		newContact.setLastName("Jones");
+		assertEquals("Jones", newContact.getLastName());
+	}
+	
+	public void testNewEmailAddress() {
+		newContact.setEmailAddress("bjones@example.com");
+		assertEquals("bjones@example.com", newContact.getEmailAddress());
+	}
+	
+	public void testNewPhoneNumber() {
+		newContact.setPhoneNumber("5555555555");
+		assertEquals("5555555555", newContact.getPhoneNumber());
+	}
+	
+	public void testInitialId() {
+		assertEquals(1L, (long) newContact.getId());
+	}
+	
+	public void testInitialFirstName() {
+		assertNotNull(newContact.getFirstName());
+	}
+	
+	public void testInitialLastName() {
+		assertNotNull(newContact.getLastName());
+	}
+	
+	public void testInitialEmailAddress() {
+		assertNotNull(newContact.getEmailAddress());
+	}
+	
+	public void testInitialPhoneNumber() {
+		assertNotNull(newContact.getPhoneNumber());
+	}
+}
---
+++ DisasterAppTest/src/com/riis/test/DisasterAppActivityTest.java
@@ -0,0 +1,117 @@
+package com.riis.test;
+
+import android.app.Instrumentation.ActivityMonitor;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.riis.DisasterAppActivity;
+import com.riis.NewContactActivity;
+import com.riis.R;
+import com.riis.SendEmergencyMessageActivity;
+import com.riis.controllers.ContactDataSource;
+import com.riis.models.Contact;
+import com.riis.models.ContactList;
+
+public class DisasterAppActivityTest extends ActivityInstrumentationTestCase2<DisasterAppActivity> {
+	
+	private Button createContactScreenButton;
+	private Button createEmergencyMessageScreenButton;
+	private TextView sampleLabel;
+	
+	private Contact contact;
+	private ContactList contactList;
+
+	public DisasterAppActivityTest() {
+		super(DisasterAppActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		DisasterAppActivity disasterAppActivity = getActivity();
+		
+		createContactScreenButton = (Button) disasterAppActivity.findViewById(R.id.createContactScreenButton);
+		createEmergencyMessageScreenButton = (Button) disasterAppActivity.findViewById(R.id.createEmergencyMessageScreenButton);
+		sampleLabel = (TextView) disasterAppActivity.findViewById(R.id.sampleLabel);
+		
+		ContactDataSource dataSource = new ContactDataSource(getActivity().getApplicationContext());
+		dataSource.open();
+		
+		contact = new Contact(dataSource.getContact().getId() + 1);
+		contact.setFirstName("Bob");
+		contact.setLastName("Jones");
+		contact.setEmailAddress("bjones@example.com");
+		contact.setPhoneNumber("5555555555");
+		
+		dataSource.close();
+		
+		contactList = new ContactList();
+	}
+	
+	public void testSampleLabelExists() {
+		assertNotNull(sampleLabel);
+	}
+	
+	public void testCreateContactScreenButtonExists() {
+		assertNotNull(createContactScreenButton);
+	}
+	
+	public void testCreateContactButtonIntent() {
+		ActivityMonitor monitor = getInstrumentation().addMonitor(NewContactActivity.class.getName(), null, true);
+		
+		TouchUtils.clickView(this, createContactScreenButton);
+		
+		monitor.waitForActivityWithTimeout(5000);
+		assertEquals(1, monitor.getHits());
+		
+		getInstrumentation().removeMonitor(monitor);
+	}
+	
+	public void testCreateEmergencyMessageButtonIntent() {
+		ActivityMonitor monitor = getInstrumentation().addMonitor(SendEmergencyMessageActivity.class.getName(), null, true);
+
+		TouchUtils.clickView(this, createEmergencyMessageScreenButton);
+		
+		monitor.waitForActivityWithTimeout(5000);
+		assertEquals(1, monitor.getHits());
+		
+		getInstrumentation().removeMonitor(monitor);
+	}
+	
+	public void testCreateContact() {
+		ContactDataSource dataSource = new ContactDataSource(getActivity().getApplicationContext());
+		dataSource.open();
+		
+		dataSource.createContact(contact);
+		Contact output = dataSource.getContact();
+		dataSource.deleteContact(output);
+		
+		dataSource.close();
+		assertEquals(output.getFirstName(), contact.getFirstName());
+		assertEquals(output.getLastName(), contact.getLastName());
+		assertEquals(output.getEmailAddress(), contact.getEmailAddress());
+		assertEquals(output.getPhoneNumber(), contact.getPhoneNumber());
+	}
+	
+	public void testGetAllContacts() {
+		ContactDataSource dataSource = new ContactDataSource(getActivity().getApplicationContext());
+		dataSource.open();
+		
+		dataSource.createContact(contact);
+		
+		Contact newContact = new Contact(contact.getId() + 1);
+		newContact.setFirstName("Joe");
+		newContact.setLastName("Smith");
+		newContact.setEmailAddress("jsmith_1@sample.com");
+		newContact.setPhoneNumber("1234567890");
+		
+		dataSource.createContact(newContact);
+		contactList.setContactList(dataSource.getContactList());
+		dataSource.deleteContact(contact);
+		
+		dataSource.close();
+		assertTrue(contactList.size() > 1);
+	}
+}

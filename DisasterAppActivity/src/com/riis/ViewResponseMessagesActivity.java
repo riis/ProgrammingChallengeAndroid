3cb398724410d9b71f4cb1b0689f44b7ee7570c8
TEK292 <tkocikjr@comcast.net>
2014-06-05 14:30:20 -0400

    Removed excess code.

--- DisasterAppActivity/src/com/riis/ViewResponseMessagesActivity.java
+++ DisasterAppActivity/src/com/riis/ViewResponseMessagesActivity.java
@@ -6,15 +6,13 @@ import android.view.View;
 import android.widget.ListView;
 
 import com.riis.controllers.ContactDataSource;
-import com.riis.controllers.ResponseMessageDataSource;
 import com.riis.controllers.ResponseMessagesAdapter;
 import com.riis.models.ContactList;
-import com.riis.models.ResponseMessage;
 
 public class ViewResponseMessagesActivity extends Activity{
 	
 	private ContactDataSource contactDataSource;
-	private ResponseMessageDataSource responseMessageDataSource;
+//	private ResponseMessageDataSource responseMessageDataSource;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState)
@@ -23,7 +21,7 @@ public class ViewResponseMessagesActivity extends Activity{
         setContentView(R.layout.response_messages_screen);
         
         contactDataSource = new ContactDataSource(this);
-        responseMessageDataSource = new ResponseMessageDataSource(this);
+//        responseMessageDataSource = new ResponseMessageDataSource(this);
         
         contactDataSource.open();
         ContactList contactList = new ContactList();
@@ -31,15 +29,15 @@ public class ViewResponseMessagesActivity extends Activity{
         contactDataSource.close();
        
         ListView listView = (ListView) findViewById(R.id.responseMessagesListView);
-        for(int i = 0; i < 15; i++) {
-        	ResponseMessage response = new ResponseMessage();
-        	response.setPhoneNumber("1234567890");
-        	response.setTextMessageContents("This is a test");
-        	response.updateMessageSentTimeStamp();
-        	responseMessageDataSource.open();
-        	responseMessageDataSource.createResponseMessage(response);
-        	responseMessageDataSource.close();
-        }
+//        for(int i = 0; i < 15; i++) {
+//        	ResponseMessage response = new ResponseMessage();
+//        	response.setPhoneNumber("1234567890");
+//        	response.setTextMessageContents("This is a test");
+//        	response.updateMessageSentTimeStamp();
+//        	responseMessageDataSource.open();
+//        	responseMessageDataSource.createResponseMessage(response);
+//        	responseMessageDataSource.close();
+//        }
         
         listView.setAdapter(new ResponseMessagesAdapter(this, contactList.getContacts()));
     }

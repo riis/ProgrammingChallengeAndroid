50d6ff9338b641096a2de99f1736fc924a48e636
TEK292 <tkocikjr@comcast.net>
2014-06-09 13:15:14 -0400

    DA-44: Per review, altered the response message screen to display only the contacts who should receive the last sent message.

--- DisasterAppActivity/src/com/riis/controllers/ResponseMessagesAdapter.java
+++ DisasterAppActivity/src/com/riis/controllers/ResponseMessagesAdapter.java
@@ -3,6 +3,7 @@ package com.riis.controllers;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,12 +44,24 @@ public class ResponseMessagesAdapter extends ArrayAdapter<Contact>{
 		
 		dataSource.close();
 		
+		boolean flag = false;
+		
 		for(int i = 0; i < responseMessages.size(); i++) {
 			if(responseMessages.get(i).getPhoneNumber().equals(values.get(position).getPhoneNumber())) {
 				timeStampView.setText(responseMessages.get(position).getTimeStamp());
 				messageView.setText(responseMessages.get(position).getTextMessageContents());
+				flag = true;
 			}
 		}
+		
+		if(!values.get(position).getMessageSentTimeStamp().equals("")) {
+			if(!flag) {
+				timeStampView.setVisibility(View.GONE);
+				messageView.setText("No messages received");
+				messageView.setGravity(Gravity.CENTER_HORIZONTAL);
+			}
+		} else if(!flag)
+			rowView.setVisibility(View.GONE);
 
 		return rowView;
 	}

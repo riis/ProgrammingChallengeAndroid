c9eb1c678557987b1fec98217dc0894ace9ff699
matwszedybyl <mwszedybyl@yahoo.com>
2014-06-11 09:39:48 -0400

    fixed name appearing without sending an emergency  message

--- DisasterAppActivity/src/com/riis/controllers/MessageIndicatorAdapter.java
+++ DisasterAppActivity/src/com/riis/controllers/MessageIndicatorAdapter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,13 +41,14 @@ public class MessageIndicatorAdapter extends ArrayAdapter<Contact>
 		
 		ResponseMessageList responseMessageList = new ResponseMessageList(context);
 		responseMessageList.read();
-		ArrayList<ResponseMessage> responseMessages = responseMessageList.getResponseMessage();
+
+
 				
 		boolean flag = false;
-		
-		for(int i = 0; i < responseMessages.size(); i++)
+		for(int i = 0; i < responseMessageList.size(); i++)
 		{
-			if(responseMessages.get(i).getPhoneNumber().equals(values.get(position).getPhoneNumber()))
+
+			if(responseMessageList.getResponseMessage(i).getPhoneNumber().equals(values.get(position).getPhoneNumber()))
 			{
 				if(values.get(position).getMessageSentTimeStamp() == 0L)
 				{
@@ -67,7 +69,7 @@ public class MessageIndicatorAdapter extends ArrayAdapter<Contact>
 		} 
 		else if(!flag)
 		{
-			indicatorView.setVisibility(View.GONE);
+			indicatorView.setVisibility(View.INVISIBLE);
 		}
 
 		return rowView;
--- DisasterAppActivity/src/com/riis/controllers/ResponseMessagesAdapter.java
+++ DisasterAppActivity/src/com/riis/controllers/ResponseMessagesAdapter.java
@@ -3,6 +3,7 @@ package com.riis.controllers;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,6 +41,7 @@ public class ResponseMessagesAdapter extends ArrayAdapter<Contact>
 		
 		ResponseMessageList responseMessages = new ResponseMessageList(context);
 		responseMessages.read();
+		Log.i("my log", "gets outside for loop" + 		values.get(position).getFirstName());
 		
 		boolean flag = false;
 		
@@ -64,7 +66,7 @@ public class ResponseMessagesAdapter extends ArrayAdapter<Contact>
 		} 
 		else if(!flag)
 		{
-			rowView.setVisibility(View.GONE);
+			rowView.setVisibility(View.INVISIBLE);
 		}
 
 		return rowView;

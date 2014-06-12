b08d7a14f94f0230ee2c65530360df53adf6a477
matwszedybyl <mwszedybyl@yahoo.com>
2014-06-12 14:59:07 -0400

    pulling down animation and dropdown lists on main screen

--- DisasterAppTest/.apt_generated/com/riis/dagger/DisasterTestObjectGraph$$ModuleAdapter.java
+++ DisasterAppTest/.apt_generated/com/riis/dagger/DisasterTestObjectGraph$$ModuleAdapter.java
@@ -25,35 +25,8 @@ public final class DisasterTestObjectGraph$$ModuleAdapter extends ModuleAdapter<
    */
   @Override
   public void getBindings(BindingsGroup bindings, DisasterTestObjectGraph module) {
-    bindings.contributeProvidesBinding("com.riis.models.TextMessageSender", new ProvideTextMessageSenderProvidesAdapter(module));
     bindings.contributeProvidesBinding("com.riis.models.ContactList", new ProvideContactListProvidesAdapter(module));
-  }
-
-  /**
-   * A {@code Binding<com.riis.models.TextMessageSender>} implementation which satisfies
-   * Dagger's infrastructure requirements including:
-   *
-   * Being a {@code Provider<com.riis.models.TextMessageSender>} and handling creation and
-   * preparation of object instances.
-   */
-  public static final class ProvideTextMessageSenderProvidesAdapter extends ProvidesBinding<com.riis.models.TextMessageSender>
-      implements Provider<com.riis.models.TextMessageSender> {
-    private final DisasterTestObjectGraph module;
-
-    public ProvideTextMessageSenderProvidesAdapter(DisasterTestObjectGraph module) {
-      super("com.riis.models.TextMessageSender", NOT_SINGLETON, "com.riis.dagger.DisasterTestObjectGraph", "provideTextMessageSender");
-      this.module = module;
-      setLibrary(false);
-    }
-
-    /**
-     * Returns the fully provisioned instance satisfying the contract for
-     * {@code Provider<com.riis.models.TextMessageSender>}.
-     */
-    @Override
-    public com.riis.models.TextMessageSender get() {
-      return module.provideTextMessageSender();
-    }
+    bindings.contributeProvidesBinding("com.riis.models.TextMessageSender", new ProvideTextMessageSenderProvidesAdapter(module));
   }
 
   /**
@@ -80,6 +53,33 @@ public final class DisasterTestObjectGraph$$ModuleAdapter extends ModuleAdapter<
     @Override
     public com.riis.models.ContactList get() {
       return module.provideContactList();
+    }
+  }
+
+  /**
+   * A {@code Binding<com.riis.models.TextMessageSender>} implementation which satisfies
+   * Dagger's infrastructure requirements including:
+   *
+   * Being a {@code Provider<com.riis.models.TextMessageSender>} and handling creation and
+   * preparation of object instances.
+   */
+  public static final class ProvideTextMessageSenderProvidesAdapter extends ProvidesBinding<com.riis.models.TextMessageSender>
+      implements Provider<com.riis.models.TextMessageSender> {
+    private final DisasterTestObjectGraph module;
+
+    public ProvideTextMessageSenderProvidesAdapter(DisasterTestObjectGraph module) {
+      super("com.riis.models.TextMessageSender", NOT_SINGLETON, "com.riis.dagger.DisasterTestObjectGraph", "provideTextMessageSender");
+      this.module = module;
+      setLibrary(false);
+    }
+
+    /**
+     * Returns the fully provisioned instance satisfying the contract for
+     * {@code Provider<com.riis.models.TextMessageSender>}.
+     */
+    @Override
+    public com.riis.models.TextMessageSender get() {
+      return module.provideTextMessageSender();
     }
   }
 }

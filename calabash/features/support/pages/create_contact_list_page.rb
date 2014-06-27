4d9b4fd5769b939ed764a3da0e924c0d485085a0
TEK292 <tkocikjr@comcast.net>
2014-06-27 16:57:27 -0400

    Created Calabash tests for contact list.

--- calabash/features/support/pages/create_contact_list_page.rb
+++ calabash/features/support/pages/create_contact_list_page.rb
@@ -12,8 +12,34 @@ class CreateContactList < Calabash::ABase
 	end
 
 	#elements
+	@@contact_list_name_edit_text = "EditText id:'contactListNameText'"
+	@@contact_list_name_error = "TextView text:'Please enter a name for the contact list!'"
+	@@contact_list_save_button = "Button id:'saveCreateContactListSaveButton'"
+	@@contact_list_cancel_button = "Button id:'cancelCreateContactListButton'"
 
 	def assert_contact_list_page
-		wait_for_elements_exist([trait], :timeout =>9)
+		wait_for_elements_exist([trait], :timeout => 9)
+	end
+
+	def input_empty_list
+		wait_for_elements_exist([trait], :timeout => 9)
+		query(@@contact_list_name_edit_text, {:setText => 'Friends'})
+	end
+
+	def input_bad_list_name
+		wait_for_elements_exist([trait], :timeout => 9)
+		query(@@contact_list_name_edit_text, {:setText => ''})
+	end
+
+	def view_list_name_error
+		query(@@contact_list_name_error)
+	end
+
+	def save_button
+		touch(query(@@contact_list_save_button))
+	end
+
+	def cancel_button
+		touch(query(@@contact_list_cancel_button))
 	end
 end
